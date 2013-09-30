7b4f2d9da85fc851f9f8918d6c25ef08d39d0204
Alex <contact@purpledonutgames.com>
2013-09-30 23:34:27 +0800

    added some basic movement code

--- motors/All.h
+++ motors/All.h
@@ -1,2 +1,11 @@
+/*
+ * All.h ~...
+ * Alex Jones for team 5105 Buffalo Wings
+ * 
+ * Description: 	includes all functions defined in the motors directory
+ * 
+ * ~licensed under the GNU GPLv3 license included in this repositiory
+*/
+
 #include "GetMotor.fn"
 #include "SetMotor.fn"

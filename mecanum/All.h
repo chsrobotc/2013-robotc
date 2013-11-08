82000edb679df2bdd602d2df5f197a79b065873a
Alex Jones <contact@purpledonutgames.com>
2013-11-08 15:50:13 -0800

    start the mecanum

---
+++ mecanum/All.h
@@ -0,0 +1,13 @@
+#pragma once
+/*
+ * All.h ~...
+ * Alex Jones for team 5105 Buffalo Wings
+ * 
+ * Description: 	includes all functions defined in the mecanum directory
+ * 
+ * ~licensed under the MIT license included in this repositiory
+*/
+
+#include "../mecanum/MoveDeg.fn"
+#include "../mecanum/RotateFixed.fn"
+#include "../mecanum/RotateDynamic.fn"

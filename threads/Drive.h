0f134838d2ea1d018ba81ecb01787766d30c7d80
Alex Jones <contact@purpledonutgames.com>
2013-12-09 15:16:12 -0800

    populated the /mod directory

--- threads/Drive.h
+++ threads/Drive.h
@@ -2,18 +2,19 @@
  /*
  * Drive.h ~ task drive()
  * Alex Jones for team 5105 Buffalo Wings
- * 
+ *
  * Description: 	The thread designated to polling and modifing the
  * 					driving motors during the teleop period.
- * 
+ *
  * Uses: 			teleop driving proccedures
- * 
- * Dependancies:	motors/All.h | controller/? | 
- * 
+ *
+ * Dependancies:	motors/All.h | controller/? |
+ *
  * ~licensed under the MIT license included in this repositiory
 */
 
 #include "../motors/All.h"
+#include "../mecanum/moveDeg.fn"
 
 task drive()
 {
@@ -22,20 +23,21 @@ task drive()
 		initController();
 		if(getJoystick(1, 1, 'y') < 5 && getJoystick(1, 1, 'y') > -5 && getJoystick(1, 1, 'x') < 5 && getJoystick(1, 1, 'x') > -5)
 			joystickMove(2, 1, 100, CUBE_SCALE);
-		else 
+		else
 		{
 			// this is when it get complicated
-			
+
 			// I need to find the magnitude and direction of the joystick
 			float xval = getJoystick(1, 2, 'x');
 			float yval = getJoystick(1, 1, 'y');
-			
+
 			// get the direction by using some trig
 			float dir = radiansToDegrees(atan2(yval, xval));
-			
-			
-			
-			moveDeg(1, 10, 100, LINNEAR_SCALE);
+
+			// get the magnitude with the pythagrean theorem
+			float mag = sqrt((xval * xval) + (yval * yval)) / 11;
+
+			moveDeg(mag, dir);
 		}
 	}
 }
